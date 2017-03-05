2688f7a807c3ba46b868256c852a23e95664283e
Felipe Jordani <faljordani@gmail.com>
2017-03-05 10:02:15 +0100

    fixed answer value to 4 - try 2

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,11 +6,9 @@ function guess() {
     //add functionality to guess function here
 }
 
-function setHiddenFields(){
-  answer.value = Math.floor(Math.random() * 10000).toString();
-  while answer.value < 4 {
-      while answer.value.length < 4{
+function setHiddenFields() {
+    answer.value = Math.floor(Math.random() * 10000).toString();
+    while answer.value < 4 {
         answer.value = "0" + answer.value;
-      }
     }
-  }
+}
