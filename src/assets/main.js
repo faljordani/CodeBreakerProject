6c1cbcede912c589980719ce80f72f01c529c421
Felipe Jordani <faljordani@gmail.com>
2017-03-05 10:08:47 +0100

    Only set the answer and attempt hidden inputs when they aren't already set

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,9 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+    if (answer.attempt == '' || answer.value == '') {
+        setHiddenFields();
+    }
 }
 
 function setHiddenFields() {
